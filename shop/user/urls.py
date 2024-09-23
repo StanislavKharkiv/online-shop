d079cb90786fbb2764d25db7ef648b9593cc7f6c
StanislavKharkiv <shtas89@gmail.com>
2024-09-23 15:47:45 +0200

    feat: add user registration and login, privat routes

--- shop/user/urls.py
+++ shop/user/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.user_index),
-    path('<int:id>/', views.user_info),
+    path("", views.user_index, name="user_index"),
+    path("user/<int:id>/", views.user_info, name="user_by_id"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
 ]
--- shop/user/views.py
+++ shop/user/views.py
@@ -1,18 +1,63 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import auth
+from django.contrib.auth.decorators import login_required
+from user.forms import CreateUserForm, LoginForm
+
 
 # Create your views here.
-
+@login_required
 def user_index(request):
-    t = render_to_string('user/index.html')
+    print(request.user.is_authenticated)
+    t = render_to_string("user/index.html")
     return HttpResponse(t)
 
+
+@login_required
 def user_info(request, id):
-    print(request.GET)
-    if id > 99: 
+    if id > 99:
         raise Http404()
     if id == 0:
-        return redirect('/user/')
-    
-    return HttpResponse(render_to_string('user/user.html', {'id': id}))
+        return redirect("/user/")
+
+    return HttpResponse(render_to_string("user/user.html", {"id": id}))
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect("user_index")
+
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+
+    return render(request, "user/register.html", {"registerform": form})
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect("user_index")
+
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("user_index")
+
+    return render(request, "user/login.html", {"loginform": form})
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("login")
