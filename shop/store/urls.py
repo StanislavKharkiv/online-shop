d7d25d371f909981e3119cc8d62d31fe3ffb218e
StanislavKharkiv <shtas89@gmail.com>
2024-11-18 16:05:13 +0100

    feat: show only current user shops

--- shop/store/urls.py
+++ shop/store/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.StoreView.as_view(), name="stores"),
     path("create/", views.CreateStoreView.as_view(), name="create_store"),
     path("<int:store_id>/", views.StoreDetailView.as_view(), name="store_detail"),
+    path("<str:username>/", views.UserStoresView.as_view(), name="user_stores"),
 ]
